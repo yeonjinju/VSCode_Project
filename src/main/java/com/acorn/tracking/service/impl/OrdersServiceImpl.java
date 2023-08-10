e61dc4c7dfdee66ee9a49d7844c6041c774b3453
yeonjinju <wnduswls21@gmail.com>
2023-08-10 18:39:38 +0900

    commit

--- src/main/java/com/acorn/tracking/service/impl/OrdersServiceImpl.java
+++ src/main/java/com/acorn/tracking/service/impl/OrdersServiceImpl.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.acorn.tracking.domain.Orders;
 import com.acorn.tracking.domain.Products;
@@ -23,30 +24,46 @@ public class OrdersServiceImpl implements OrdersService {
 
     private final OrdersMapper ordersMapper;
     private final ProductsMapper productsMapper;
-
+    
     @Override
-    @Scheduled(initialDelay = 5000, fixedRate = 10000)
+    @Scheduled(initialDelay = 5000, fixedRate = 10000) // 어플리케이션 시작되면 5초뒤에 자동실행 10초마다 재실행된다.
+    @Transactional // 메소드안에 메퍼를 두개 끌고온다. 안붙혀도되지만, 붙히면 이메소드안에서 실행하면 둘중 하나만 오류떠도 롤백이뜬다. 둘다성공하면 커밋.
     public void autoInsertOrders() {
+        Faker faker = createFakerInstance();
+        Products.ProductInfo productInfo = getRandomProductInfo(faker);
+        List<Integer> productIds = getProductIds(productInfo);
+        Orders orders = createOrder(productInfo, productIds.size());
+        insertOrders(orders, productIds);
+    }
+
+    private Faker createFakerInstance() {
         // 가짜 데이터를 생성하기 위해 한국 로캘을 사용하여 Faker 인스턴스 생성
-        Faker faker = new Faker(new Locale("ko"));
+        return new Faker(new Locale("ko"));
+    }
+
+    private Products.ProductInfo getRandomProductInfo(Faker faker) {
         // productsMapper를 사용하여 랜덤한 제품 정보를 가져옴
-        Products.ProductInfo productInfo = productsMapper
-                .getRandomProductsInfo(faker.random().nextInt(1, 5));
+        return productsMapper.getRandomProductsInfo(faker.random().nextInt(1, 5));
+    }
+
+    private List<Integer> getProductIds(Products.ProductInfo productInfo) {
         // productInfo에서 쉼표로 구분된 제품 ID들을 문자열로 가져옴
         // 쉼표로 구분된 문자열을 정수 리스트로 변환
-        List<Integer> productIds = Arrays
-                .stream(productInfo.getProduct_id().split(","))
+        return Arrays.stream(productInfo.getProduct_id().split(","))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
+    }
+
+    private Orders createOrder(Products.ProductInfo productInfo, int quantity) {
         // 생성된 가짜 데이터로 Orders 객체 생성
-        Orders orders = Orders
-                .builder()
+        return Orders.builder() // setter=builder
                 .admin_id(1) // 관리자 ID 설정
-                .quantity_ordered(productIds.size())
+                .quantity_ordered(quantity) // 상품 총 개수
                 .total_price(productInfo.getTotal_price()) // productInfo에서 총 가격 설정
-                .customer_name(faker.name().fullName()) // 가짜 고객 이름 생성
-                .delivery_address(faker.address().fullAddress()) // 가짜 배송 주소 생성
                 .build();
+    }
+
+    private void insertOrders(Orders orders, List<Integer> productIds) {
         // ordersMapper를 사용하여 주문 데이터베이스에 삽입
         ordersMapper.autoInsertOrders(orders);
         // 각 제품의 재고를 productsMapper를 사용하여 감소
--- src/main/java/com/acorn/tracking/service/impl/ProductsServiceImpl.java
+++ src/main/java/com/acorn/tracking/service/impl/ProductsServiceImpl.java
@@ -13,35 +13,45 @@ import com.acorn.tracking.adapter.LocalDateTimeAdapter;
 import com.acorn.tracking.domain.Products;
 import com.acorn.tracking.mapper.ProductsMapper;
 import com.acorn.tracking.service.ProductsService;
-import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
 import lombok.RequiredArgsConstructor;
 
-@Service
-@RequiredArgsConstructor
+@Service // 서비스 클래스라는걸 정의(인식)
+@RequiredArgsConstructor // final (auto안써도됌)
 public class ProductsServiceImpl implements ProductsService {
 
     private final ProductsMapper productsMapper;
 
     @Override
-    public void loadProductsFromFile() throws IOException {
-        // LocalDateTime에 대한 사용자 지정 타입 어댑터를 등록하여 Gson 인스턴스를 생성합니다.
-        Gson gson = new GsonBuilder()
-                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
-                .create();
+    public void loadProductsFromFile() throws IOException { // 이곳에서 입셉션을 호출한곳에 던지는 (입센션에서 처리하는 정의 단위)
+        InputStream inputStream = getProductsJsonInputStream(); // 내부에 담긴 json으로 된 파일을 읽어와서 인풋스트림에 넣음
+        List<Products> products = readProductsFromJson(inputStream);
+        insertProductsIntoDatabase(products);
+    }
+
+    private InputStream getProductsJsonInputStream() throws FileNotFoundException {
         // "static" 디렉토리에 있는 "Products.json" 파일의 입력 스트림을 가져옵니다.
         InputStream inputStream = getClass().getResourceAsStream("/static/Products.json");
         // 입력 스트림이 null인지 확인하여 파일을 찾을 수 없는 경우 예외를 던집니다.
         if (inputStream == null) {
             throw new FileNotFoundException("File not found: Products.json");
         }
-        // 리더를 생성하여 입력 스트림의 내용을 읽습니다.
-        InputStreamReader reader = new InputStreamReader(inputStream);
-        // Gson을 사용하여 JSON 내용을 Products 객체의 리스트로 역직렬화합니다.
-        List<Products> products = gson.fromJson(reader, new TypeToken<List<Products>>() {
-        }.getType());
+        return inputStream;
+    }
+
+    private List<Products> readProductsFromJson(InputStream inputStream) {
+        // Gson 인스턴스를 생성하고 LocalDateTime에 대한 사용자 지정 타입 어댑터를 등록합니다.
+        // 리더를 사용하여 입력 스트림의 내용을 읽습니다.
+        return new GsonBuilder()
+                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+                .create().fromJson(
+                        new InputStreamReader(inputStream),
+                        new TypeToken<List<Products>>() {}.getType());
+    }
+
+    private void insertProductsIntoDatabase(List<Products> products) {
         // 제품 목록을 반복하며 각 제품을 매퍼를 통해 데이터 저장소에 삽입합니다.
         for (Products product : products) {
             productsMapper.autoInsertProducts(product);
